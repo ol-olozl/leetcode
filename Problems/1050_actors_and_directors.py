fffb80ad25d5410d71584513b5d846cebb65161e
Zoey Lee <olozl1228@gmail.com>
2025-06-09 22:51:20 +0900

    Create 1050_actors_and_directors.py
    https://leetcode.com/problems/actors-and-directors-who-cooperated-at-least-three-times/description/

---
+++ Problems/1050_actors_and_directors.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+    df = actor_director[["actor_id", "director_id"]].value_counts().reset_index()
+    df = df[df["count"]>=3]
+    return df[["actor_id", "director_id"]]
